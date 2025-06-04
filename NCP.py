291ae3858c28e9181879b644eee71f0712924628
Kirin2D2 <kirin@useunix.com>
2025-06-04 10:55:39 -0400

    fix ablate and transpose

--- NCP.py
+++ NCP.py
@@ -6,9 +6,9 @@ from torch import nn
 #### Take as input a subspace matrix U of dimension DxD, a list of 
 # dimensions of each DRSA subspace, and a list of irrelevant subspace
 # indices (zero indexed). Returns ablated weight matrices U_ab and U_ab_transpose.
-def ablate_subspace_matrix(U, subspace_dims, irrelevant_subspaces):
-    U_ab = U.copy()
-    U_ab_transpose = U.copy().T
+def ablate_subspace_matrix(U: torch.Tensor, subspace_dims: list[int], irrelevant_subspaces: list[int]):
+    U_ab = U.clone()
+    U_ab_transpose = U.t().clone()
 
     for k_prime in irrelevant_subspaces:
          start_dim = sum(subspace_dims[:k_prime])
