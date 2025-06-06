b3454ccaaf803be987df71a4eca539565514ed3e
Kirin2D2 <kirin@useunix.com>
2025-06-06 16:21:34 -0400

    trying to implement explain_augmented_network

--- test_basketball_ablasion.py
+++ test_basketball_ablasion.py
@@ -4,13 +4,19 @@ import NCP
 SUBSPACE_DIMS = [128, 128, 128, 128]
 IRRELEVANT_SUBSPACES = [3]
 U_FILEPATH = '/u/kd9132/n/fs/ncp/NCP.v2/data/projection_matrices/U_basketball_tensor.pt'
+IMAGE_FILEPATH = '/u/kd9132/n/fs/ncp/NCP.v2/data/images/drsa-basketball-img3.jpg'
+CLASS_NAME = 'basketball'
 
 ### load tensor U 
 U = torch.load(U_FILEPATH)
 # ablate and transpose
 U_ab, U_ab_T = ablate_subspace_matrix(U, SUBSPACE_DIMS, IRRELEVANT_SUBSPACES)
+U_ab = U_ab.cuda()
+U_ab_T = U_ab_T.cuda()
 
 ### augment VGG16 with ablated matrix
-raise NotImplementedError
+augmentedVGG16 = AugmentedVGG16(U=U_ab, UT=U_ab_T).cuda()
+augmentedVGG16.eval()
 
-### generate LRP heatmap of augmented VGG16 matrix
+### generate LRP heatmap of augmented VGG16 on drsa basketball image 3
+raise NotImplementedError
