ea601d1e4b6bea57a6d37ea8ed6b4c81d92b02f3
Kirin2D2 <kirin@useunix.com>
2025-06-08 19:31:33 -0400

    gpu not working

--- test_basketball_ablasion.py
+++ test_basketball_ablasion.py
@@ -69,7 +69,8 @@ def visualize_and_save_lrp(orig_pil: Image.Image,
 
 
 if __name__ == "__main__":
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    #device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cpu')
 
     ### 1. Load tensor U, ablate and move to GPU
     U = torch.load(U_FILEPATH)  # shape: (512, 512)
