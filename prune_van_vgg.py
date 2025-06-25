12c525e397cbf2eddb34b69410d54e0453085477
Kirin2D2 <kirin@useunix.com>
2025-06-25 10:16:23 -0400

    add some funcs from lrp pruning github

--- prune_van_vgg.py
+++ prune_van_vgg.py
@@ -12,6 +12,7 @@ from torch.autograd import Variable, Function
 
 from lrp import *
 from prune_layer import *
+import data as dataset
 
 from operator import itemgetter
 from heapq import nsmallest
@@ -121,6 +122,7 @@ class FilterPruner:
                 self.filter_ranks[i] = v.cpu()
     
     # re index the filters to prune so that indices align during pruning
+    # return list of (layer_num, filter_num)
     def get_pruning_plan(self, num_filters_to_prune):
         filters_to_prune = self.lowest_ranking_filters(num_filters_to_prune)
         #filters_to_prune: filters to be pruned 1) layer number, 2) filter number, 3) its value
@@ -155,3 +157,233 @@ class FilterPruner:
                 data.append((self.activation_to_layer[i], j, self.filter_ranks[i][j]))
         # return num tuples w/ smallest relevance score
         return nsmallest(num, data, itemgetter(2)) 
+    
+class PruningFineTuner:
+    def __init__(self, args, model):
+        torch.manual_seed(args.seed)
+        if args.cuda:
+            torch.cuda.manual_seed(args.seed)
+
+        self.args = args
+        self.setup_dataloaders()
+        self.model = model
+
+        self.criterion = nn.CrossEntropyLoss()
+        self.pruner = FilterPruner(self.model, args)
+        self.model.train() ### DEBUG: necessary?
+        self.save_loss = False
+
+    def setup_dataloaders(self):
+        from torchvision import datasets, transforms
+        kwargs = {'num_workers': 1, 'pin_memory': True} if self.args.cuda else {}
+        
+        train_dataset = datasets.ImageFolder()
+        # Data Acquisition
+        get_dataset = {
+            #"cifar10": dataset.get_cifar10,  # CIFAR-10
+                'imagenet': dataset.get_imagenet, # ImageNet
+                #'basketball_imagenet': dataset.get_basketball_imagenet
+        }[self.args.data_type.lower()]
+        train_dataset, test_dataset = get_dataset()
+        print(f"train_dataset:{len(train_dataset)}, test_dataset:{len(test_dataset)}")
+        # Data Loader (Input Pipeline)
+        self.train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                        batch_size=self.args.train_batch_size,
+                                                        shuffle=True, **kwargs)
+
+        self.test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                                    batch_size=self.args.test_batch_size,
+                                                    shuffle=False, **kwargs)
+
+        self.train_num = len(self.train_loader)
+        self.test_num = len(self.test_loader)
+
+    def test(self):
+        self.model.eval()
+        test_loss = 0
+        correct = 0
+
+        for data, target in self.test_loader:
+            if self.args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            output = self.model(data)
+
+            test_loss += self.criterion(output, target).item()
+            # get the index of the max log-probability
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+        test_loss /= len(self.test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(self.test_loader.dataset),
+            100. * correct / len(self.test_loader.dataset)))
+        # self.correct += correct
+
+        if self.save_loss:
+            self.test_acc_tot.append((100. * correct).numpy() / len(self.test_loader.dataset))
+            self.test_loss_tot.append(test_loss)
+            self.test_iter.append(self.niter)
+
+            # FLOP calculation
+            sample_batch = torch.FloatTensor(1, 3, 224, 224).cuda()
+            self.model = add_flops_counting_methods(self.model)
+            self.model.eval().start_flops_count()
+            _ = self.model(sample_batch)
+            self.flop_val.append(flops_to_string(self.model.compute_average_flops_cost()))
+            self.num_param.append(get_model_parameters_number(self.model))
+            print('Flops:  {}'.format(flops_to_string(self.model.compute_average_flops_cost())))
+            print('Params: ' + get_model_parameters_number(self.model))
+
+        self.model.train()
+
+    def train_epoch(self, optimizer=None, rank_filters=False):
+        self.train_loss_batch = 0
+        for batch_idx, (data, target) in enumerate(self.train_loader):
+            if self.args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            self.train_batch(optimizer, batch_idx, data, target, rank_filters)
+
+        if self.save_loss:
+            self.train_loss_tot.append(self.train_loss_batch / len(self.train_loader.dataset))
+
+    def train_batch(self, optimizer, batch_idx, batch, label, rank_filters):
+        self.model.zero_grad()
+
+        if rank_filters:
+            if self.args.relevance:  # lrp_based
+                output = self.prunner.forward_lrp(batch)
+
+                T = torch.zeros_like(output)
+                for ii in range(len(label)):
+                    T[ii,label[ii]] = 1.0
+
+                self.prunner.backward_lrp(T.data)
+
+                print('Train Epoch: [{}/{} ({:.0f}%)]'.format(
+                    batch_idx * len(batch), len(self.train_loader.dataset),
+                    100. * batch_idx / len(self.train_loader)))
+                loss = self.criterion(output, label)
+                self.train_loss_batch += loss.item()
+
+            else:  # gradient_based
+                output = self.prunner.forward(batch)
+                loss = self.criterion(output, label)
+                loss.backward()
+                print('Train Epoch: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    batch_idx * len(batch), len(self.train_loader.dataset),
+                    100. * batch_idx / len(self.train_loader), loss.item()))
+                self.train_loss_batch += loss.item()
+        else:
+            loss = self.criterion(self.model(batch), label)
+            loss.backward()
+            optimizer.step()
+            print('Train Epoch: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                batch_idx * len(batch), len(self.train_loader.dataset),
+                100. * batch_idx / len(self.train_loader), loss.item()))
+            self.train_loss_batch += loss.item()
+
+    def total_num_filters(self):
+        # Conv layer의 모든 filter 수를 counting
+        filters = 0
+        for name, module in self.model.features._modules.items():
+            if isinstance(module, torch.nn.modules.conv.Conv2d):
+                filters = filters + module.out_channels
+        return filters
+
+    def train(self, optimizer=None, epoches=10):
+        if optimizer is None:
+            optimizer = optim.SGD(self.model.classifier.parameters(), lr=self.args.lr, momentum=self.args.momentum)
+
+        # self.correct = 0
+        for i in range(epoches):
+            print("Epoch: ", i)
+            self.train_epoch(optimizer)
+            self.test()
+        print("Finished fine tuning")
+        # self.correct /= epoches
+
+    def get_candidates_to_prune(self, num_filters_to_prune):
+        self.prunner.reset()
+
+        self.train_epoch(rank_filters=True)
+        # training 하면서 동시에 hook 써서 후보 찾기 #
+        # (각 layer 마다 compute_rank 안에서 계산되어서 self.filter_ranks list에 저장이 된다.
+
+        self.prunner.normalize_ranks_per_layer()  # Normalization
+
+        return self.prunner.get_prunning_plan(num_filters_to_prune)
+
+    def forward_hook(self):
+        # For Forward Hook
+        for name, module in self.model.features._modules.items():
+            module.register_forward_hook(fhook)
+        for name, module in self.model.classifier._modules.items():
+            module.register_forward_hook(fhook)
+
+    def prune(self):
+        self.train_loss_tot = []
+        self.test_loss_tot = []
+        self.test_acc_tot = []
+        self.test_iter = []
+        self.flop_val =[]
+        self.num_param = []
+        self.R_tot = []
+        self.data_tot = []
+        self.time_tot = []
+        self.save_loss = True
+
+        # Get the accuracy before prunning
+        self.niter = 0
+        self.temp = 0
+        self.test()
+        self.model.train()
+
+        # Make sure all the layers are trainable
+        for param in self.model.features.parameters():
+            param.requires_grad = True
+
+        number_of_filters = self.total_num_filters()
+        num_filters_to_prune_per_iteration = int(number_of_filters * self.args.pr_step)  # 0.05 (5%) -> 0.01 (1%) temporally
+        iterations = int(float(number_of_filters) / num_filters_to_prune_per_iteration)
+        iterations = int(iterations * self.args.total_pr) #up to 80%
+
+        # # print("Number of prunning iterations to reduce 67% filters", iterations)
+
+        R_tot, data_tot, time_tot = self.lrp()  # lrp using conventional model
+        self.R_tot.append(R_tot)
+
+        for kk in range(iterations):
+            print("Ranking filters.. {}".format(kk))
+            self.niter += 1
+            prune_targets = self.get_candidates_to_prune(num_filters_to_prune_per_iteration)
+            # prune_targets: 잘라야 할 filter들의 1) layer number, 2) filter number가 넘어옴
+            layers_prunned = {}
+            for layer_index, filter_index in prune_targets:
+                if layer_index not in layers_prunned:
+                    layers_prunned[layer_index] = 0
+                layers_prunned[layer_index] += 1
+
+            print("Layers that will be prunned", layers_prunned)  # 총 잘릴 layer 별 filter 수
+            print("Prunning filters.. ")
+            model = self.model.cpu()  # 현재 모델 갖다가..
+            for layer_index, filter_index in prune_targets:  # 하나씩 꺼내서 자르기 시작
+                model = prune_conv_layer_sequential(model, layer_index, filter_index, cuda_flag=self.args.cuda)
+
+            self.model = model.cuda() if self.args.cuda else model
+
+            message = str(100 * float(self.total_num_filters()) / number_of_filters) + "%"
+            print("Filters prunned", str(message))
+            self.test()  # 잘리고 나서 test 해봄
+            print("Fine tuning to recover from prunning iteration.")
+            optimizer = optim.SGD(self.model.parameters(), lr=self.args.lr, momentum=self.args.momentum)
+            self.train(optimizer, epoches=10)
+            R_tot, data_tot, time_tot = self.lrp()
+            self.R_tot.append(R_tot)
+            del R_tot
+
+        print("Finished. Going to fine tune the model a bit more")
+        self.niter += 1
+        self.train(optimizer, epoches=15)
+
